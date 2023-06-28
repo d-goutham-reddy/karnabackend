2aefd44450b7a6241eec08687ecf04fa385c4c65
Goutham Reddy D <Goutham@gascitech.ml>
2023-06-28 18:37:28 +0530

    Geolocator

--- routes/HospitalRoutes.js
+++ routes/HospitalRoutes.js
@@ -1424,7 +1424,7 @@ router.post("/geolocator",async(req,res)=>{
         //   }
         //   Geolocator.findByIdAndUpdate(geol[0]._id,{})
         // })
-        Geolocator.findOneAndUpdate({},{lat:req.body.lat,long:req.body.long,speed:req.body.speed,alt:req.body.alt},{new:true},function(err,geo){
+        Geolocator.findOneAndUpdate({},{lat:req.body.lat,long:req.body.long,speed:req.body.speed},{new:true},function(err,geo){
           if(err){
             res.status(502).json(err);
           }
